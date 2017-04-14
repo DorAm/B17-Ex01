c80acecd90d90ba1a5ebdb66891e9e8701b97c80
DorAmar <bndor.am@gmail.com>
2017-04-14 18:17:10 +0300

    get input numbers and validate (section1)

--- B17 Ex01 DorAmar 301113403 OriReshef 203319900/B17_Ex01_1/Binary_Series.cs
+++ B17 Ex01 DorAmar 301113403 OriReshef 203319900/B17_Ex01_1/Binary_Series.cs
@@ -1,8 +1,34 @@
 ﻿
 namespace B17_Ex01_1
 {
-    class Binary_Series
+    class Program
     {
-        // Hello Ori
+        public static void Main(string[] args)
+        {
+            getNumberFromUser();
+        }
+        private static float getNumberFromUser()
+        {
+            bool isValidInput = true;
+            float inputNumber;
+            do
+            {
+                string messageToUser = isValidInput ? "please enter a three digit natural number\n" :
+                                                      "Error!\nplease enter a valid input\n";
+                System.Console.WriteLine(messageToUser);
+
+                inputNumber = float.Parse(System.Console.ReadLine());
+                isValidInput = is3DigitNaturalNumber(inputNumber);
+            } while (!isValidInput);
+
+            return inputNumber;
+        }
+        private static bool is3DigitNaturalNumber(float i_inputNumber)
+        {
+            return (100 <= i_inputNumber && i_inputNumber <= 999) &&
+                (i_inputNumber % 1 == 0);
+        }
+     
     }
+    
 }
