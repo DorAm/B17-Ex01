67c25fdd47e4931131e1fc72d3d49b1f7515d402
DorAmar <bndor.am@gmail.com>
2017-04-15 01:50:55 +0300

    added input

--- B17 Ex01 DorAmar 301113403 OriReshef 203319900/B17_Ex01_5/Program.cs
+++ B17 Ex01 DorAmar 301113403 OriReshef 203319900/B17_Ex01_5/Program.cs
@@ -10,15 +10,41 @@ namespace B17_Ex01_5
             short minDigitInNumber = 0, maxDigitInNumber = 0;
             short io_greaterThanFirstDigitCounter = 0, io_smallerThanFirstDigitCounter = 0;
 
+            int inputNumber = getNumberFromUser();
 
-            findMinAndMaxDigit(12345678, ref minDigitInNumber, ref maxDigitInNumber);
+            findMinAndMaxDigit(inputNumber, ref minDigitInNumber, ref maxDigitInNumber);
             Console.WriteLine("The maximum digit in the given number is " + maxDigitInNumber);
             Console.WriteLine("The minimum digit in the given number is " + minDigitInNumber);
 
-            findNumberOfDigitsGreaterAndSmallerThenFirstDigit(12345678, ref io_greaterThanFirstDigitCounter, ref io_smallerThanFirstDigitCounter);
+            findNumberOfDigitsGreaterAndSmallerThenFirstDigit(inputNumber, ref io_greaterThanFirstDigitCounter, ref io_smallerThanFirstDigitCounter);
             Console.WriteLine("The number of digits greater then the first digit is " + io_greaterThanFirstDigitCounter);
             Console.WriteLine("The number of digits smaller then the first digit is " + io_smallerThanFirstDigitCounter);
 
+        }
+
+        private static int getNumberFromUser()
+        {
+            bool isValidInput = true;
+            int inputNumber;
+
+            do
+            {
+                string messageToUser = isValidInput ? "please enter an eight digit natural number" :
+                                                      "Error! Please enter a valid input\n";
+                System.Console.WriteLine(messageToUser);
+
+                inputNumber = int.Parse(System.Console.ReadLine());
+                isValidInput = is8DigitNaturalNumber(inputNumber);
+            } while (!isValidInput);
+
+            return inputNumber;
+        }
+
+        private static bool is8DigitNaturalNumber(float i_InputNumber)
+        {
+
+            return (10000000 <= i_InputNumber && i_InputNumber <= 99999999) &&
+                (i_InputNumber % 1 == 0);
         }
 
         public static void findMinAndMaxDigit(int i_NaturalNumber, ref short io_MinDigit, ref short io_MaxDigit)
