7597ffc5d47ef1eaadc3183a89053f63cc902d78
Ori Reshef <ori.reshef32@gmail.com>
2017-04-16 15:58:34 +0300

    finished sections 2&3

--- B17 Ex01 DorAmar 301113403 OriReshef 203319900/B17_Ex_01_3/Program.cs
+++ B17 Ex01 DorAmar 301113403 OriReshef 203319900/B17_Ex_01_3/Program.cs
@@ -1,4 +1,5 @@
-﻿using B17_Ex_01_2;
+﻿using System;
+using B17_Ex_01_2;
 
 namespace B17_Ex_01_3
 
@@ -7,8 +8,25 @@ namespace B17_Ex_01_3
     {
         public static void Main()
         {
-            B17_Ex_01_2.Program.PrintSandClock();
-            System.Console.ReadLine();
+            string userInput;
+
+            do
+            {
+                System.Console.WriteLine("Hello! please enter your clock hight - a positive natural number.");
+                userInput = System.Console.ReadLine();
+            } while (!checkUserInput(userInput));
+
+            short sandClockHight = short.Parse(userInput);
+            sandClockHight = (sandClockHight % 2) == 0 ? (short)(sandClockHight + 1) : (short)(sandClockHight + 0) ;
+           
+            B17_Ex_01_2.Program.PrintSandClock(sandClockHight);
+            
+        }
+
+        private static bool checkUserInput(string i_UserInput)
+        {
+            float testedInput = float.Parse(i_UserInput);
+            return testedInput > 0 && (testedInput % 1) == 0;
         }
     }
 }
